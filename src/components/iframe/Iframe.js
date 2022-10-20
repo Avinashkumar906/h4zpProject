afa91f73a014dee0f31f11d0d8ce8710bc4206f7
Avi Nash <avinashkumar906@gmail.com>
2022-10-20 17:37:00 +0530

    iframe fix

--- src/components/iframe/Iframe.js
+++ src/components/iframe/Iframe.js
@@ -2,26 +2,25 @@ import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 function Iframe(props) {
-  const [src,setSrc] = useState(props.data?.src);
+  const [iframe, setIframe] = useState();
+  const [src] = useState(props.data?.src);
+  const [ autoplay ] = useState(props.data?.autoplay);
+  const [ mute ] = useState(props.data?.mute);
   
   useEffect(() => {
     let suffix = src.indexOf('?') === -1 ? '?': '';
-
-    if(props.data?.autoplay)
-      suffix+=`&autoplay=1`;
-    if(props.data?.mute)
-      suffix+=`&mute=1`;
-
-    setSrc(src+suffix);
+    if(autoplay) suffix+=`&autoplay=1`;
+    if(mute) suffix+=`&mute=1`;
+    setIframe(src+suffix);
     return () => console.log('clean iframe')
-  }, [props.data.autoplay,props.data?.mute])
+  }, [autoplay, mute, src])
   
   return (
     <Container className={`p-0 ${props.data?.theme}`} fluid>
       <Container fluid={props.data?.fluid}>
         <Row style={{ minHeight: '560px' }}>
           <Col>
-            <iframe title="this is iframe" src={src} allow='autoplay' className="w-100 h-100" scrolling="no" frameBorder="0" allowFullScreen></iframe>
+            <iframe title="this is iframe" src={iframe} allow='autoplay' className="w-100 h-100" scrolling="no" frameBorder="0" allowFullScreen></iframe>
           </Col>
         </Row>
       </Container>
