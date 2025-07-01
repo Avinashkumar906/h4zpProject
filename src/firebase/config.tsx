4b9d36b54e39aa111abb50dae12244443616bce5
Avi Nash <avinashkumar906@gmail.com>
2025-07-01 21:22:06 +0530

    cloudinary fix

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,11 +1,3 @@
-// export const FIREBASE_CONFIG = {
-//   apiKey: "AIzaSyDLBLSiCUNlWscO2CN-9iKvs93W_ZeIpZI",
-//   authDomain: "h4zp-india.firebaseapp.com",
-//   projectId: "h4zp-india",
-//   storageBucket: "h4zp-india.appspot.com",
-//   messagingSenderId: "395864357202",
-//   appId: "1:395864357202:web:a7ee3381d2f79ba891628b"
-// };
 export const FIREBASE_CONFIG = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
