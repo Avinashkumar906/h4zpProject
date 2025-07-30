498e3c22a0b9373a8378ea3dbd91016e93b65eb4
Avi Nash <avinashkumar906@gmail.com>
2025-07-30 19:26:44 +0530

    position fixed changed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,8 +5,8 @@ import { ReactNode } from 'react';
 function NotFound({ children }: { children: ReactNode }) {
   return (
     <div
-      className="d-flex position-absolute inset-0 justify-content-center align-items-center bg-lilac"
-      style={{ zIndex: 2000, position: 'absolute', inset: 0, height: '100dvh', width: '100vw' }}
+      className="d-flex position-fixed inset-0 justify-content-center align-items-center bg-lilac"
+      style={{ zIndex: 2000, inset: 0, height: '100dvh', width: '100vw' }}
     >
       <div className="text-center">
         <div>{children}</div>
